8d0aaf8892e69453a6bbc2c6cf4f16907579f05d
wcoding <papa5944@gmail.com>
2024-07-29 15:45:54 +0900

    book REST API completed

---
+++ frontend/src/api/books.js
@@ -0,0 +1,52 @@
+// TODO: Import API_ENDPOINT
+import {API_ENDPOINT} from './index';
+
+// TODO: Create the addNewBook function that takes in newTitle, newStart, and newEnd as arguments. Inside the function, create a POST request for the new book. Store the response as a JSON in a variable called newBook and return it at the end of the function.
+
+export const addNewBook = async (newTitle, newStart, newEnd)=>{
+   const response = await fetch(`${API_ENDPOINT}/books`,{
+        method: 'POST',
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({
+            title: newTitle,
+            start: newStart,
+            end: newEnd
+        })
+
+    });
+
+    const newBook = response.json()
+    return newBook;
+}
+
+// TODO: Create the getBooks function that retrieves all of the books that have been saved to the back-end API
+export const getBooks = async ()=>{
+    const response = await fetch(`${API_ENDPOINT}/books`);
+    const books =  await response.json();
+    return books;
+
+}
+// TODO: Create the updateBook function that takes the arguments id, newTitle, newStart, newEnd. Inside of the function, create a PUT request for the specified book to be updated. Return the status of the response at the end of the function.
+export const updateBook = async (id, newTitle, newStart, newEnd)=>{
+    const response = await fetch(`${API_ENDPOINT}/books/${id}`,{
+        method: 'PUT',
+        headers:{
+            "Content-type": "application/json",
+        },
+
+        body: JSON.stringify({
+            title: newTitle,
+            start: newStart,
+            end: newEnd
+        })
+    })
+
+    return response.status;
+}
+// TODO: Create the deleteBook function that takes the id of the book to be deleted as an argument. Inside of the function, create a DELETE request for the specified book to be deleted. Return the status of the response at the end of the function.
+export const deleteBook = async (id)=>{
+    const response = await fetch(`${API_ENDPOINT}/books/id`,{
+        method: 'DELETE',
+    });
+   return response.status;
+}
